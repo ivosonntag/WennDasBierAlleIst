bcd1b05f8a27e5d747cbe7f0801da5a1bb64c289
Sebastian Werling <sebastian.werling@gmail.com>
2018-09-27 22:45:01 +0200

    bugfix in data_sql and now retweets are not truncated anymore

--- twitter/tweet_streamer.py
+++ twitter/tweet_streamer.py
@@ -31,11 +31,12 @@ def weighted(value):
 
 def format_tweet_data(tweet):
     sentiment = None
-    #data = json.loads(tweet)
     data = tweet._json
     # added to get the full text if status exceeds 140 characters
     if "extended_tweet" in data:
         text = data['extended_tweet']['full_text']
+    elif 'retweeted_status' in data and "extended_tweet" in data['retweeted_status']:
+        text = data['retweeted_status']['extended_tweet']['full_text']
     else:
         text = data["text"]
     if "sentiment" in config.get('TWITTER', 'include_data').split(', '):
