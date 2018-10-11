acbae9f0a8980e4044aa37939bc553dfe228b806
Fabian Gebhart <fabian.gebhart@gmail.com>
2018-10-11 23:22:52 +0200

    update "All_Tweets" table deleted tweets to True

--- twitter/deletion_checker.py
+++ twitter/deletion_checker.py
@@ -5,7 +5,8 @@ import configparser
 
 import tweepy
 
-from utils.data_storage import StorageFactory
+from utils.dict2sql import Dict2Sql
+
 from utils.helper_functions import authenticate,\
     create_db_connection,\
     get_time,\
@@ -74,11 +75,13 @@ if __name__ == '__main__':
     tweet_attributes = config.get("DELETION", "include_attributes").split(', ')
     user_attributes = config.get("DELETION", "user_attributes").split(', ')
     full_path_to_file = os.path.join(path_to_data, file_name)
+    path_to_db = os.path.join(path_to_data, db_name)
 
     # build logger
     logger = build_logger("deletion-checker", log_level)
 
-    storage = StorageFactory.create(config)
+    # initiate Dict2Sql
+    sql_db = Dict2Sql(path_to_db)
 
     while True:
 
@@ -87,7 +90,6 @@ if __name__ == '__main__':
         api = tweepy.API(auth)
 
         # get 'tweet_id's and 'created_at' attribute
-        path_to_db = os.path.join(path_to_data, db_name)
         logger.info("checking deleted tweets of: {}".format(path_to_db))
 
         # following line returns a huge list. Slice it for testing purpose
@@ -107,15 +109,20 @@ if __name__ == '__main__':
             offline_tweet_ids, still_online_tweets_data = check_deletion(batch)
 
             for online_tweet in still_online_tweets_data:
-                storage.save(online_tweet, "checked_tweets")
+                sql_db.save(online_tweet, "checked_tweets", True, True)
 
-            # TODO: Check "All_Tweets" table for IDs in offline_tweet_ids and update deleted value to True
+            for offline_id in offline_tweet_ids:
+                filter_dict = set_values = dict()
+                filter_dict['id_str'] = offline_id
+                set_values['deleted'] = True
+                logger.debug("setting deleted to True for id: {}".format(offline_id))
+                sql_db.update_with_dict_filter(set_values, "All_Tweets", filter_dict)
 
             logger.debug("number of offline tweets found in this batch: {}".format(len(offline_tweet_ids)))
             logger.debug("offline tweet ids: {}".format(offline_tweet_ids))
 
         logger.info("done checking status of tweets! - now waiting for next round...")
-        logger.debug("---------------------------------------------------------------")
+        logger.debug("--------------------------------------------------------------")
         while datetime.datetime.now().minute % interval != 0:
             # sleeping
             # print("sleeping")
